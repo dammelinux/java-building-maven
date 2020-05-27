128c95586b03d439c27ca3beeb4210cb2b432c40
trongthongvo <56016681+trongthongvo@users.noreply.github.com>
2020-05-27 11:45:54 +0700

    update notify email

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Learn today lead tomorrow/ how are u today");
+		get("/hello", (req, res) -> "Learn today lead tomorrow/ Learn something everyday");
 	}
 }
